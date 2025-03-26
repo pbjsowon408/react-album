42d2c7c0ede285de316499dbca84e6bff80072bc
Eddie Park <pbjsowon408@gmail.com>
2025-03-26 23:57:05 +0900

    [ADD] route function

--- react-album/src/App.tsx
+++ react-album/src/App.tsx
@@ -1,5 +1,20 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MainPage from "@pages/index"
+import About from "@pages/about"
+
 function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route index path='/' element={<MainPage/>}/>
+      <Route index path='/about' element={<About/>}/>
+      </Routes>
+  </BrowserRouter>
   return <div>App 컴포넌트 화면입니다.</div>;
-}
+};
 
 export default App;
