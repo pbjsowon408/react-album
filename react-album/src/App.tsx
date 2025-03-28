fdbbdd3d712eab3c3780ad9b2f0d8f982366dd45
Eddie Park <pbjsowon408@gmail.com>
2025-03-28 23:56:29 +0900

    [ADD] add common scss style

--- react-album/src/App.tsx
+++ react-album/src/App.tsx
@@ -5,14 +5,12 @@ import {
   Route,
 } from "react-router-dom";
 import MainPage from "@/pages/index/index"
-import About from "@pages/about"
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route index path='/' element={<MainPage />}/>
-        <Route path='/about:id' element={<About />}/>
       </Routes>
     </BrowserRouter>
   );
--- react-album/src/pages/index/index.tsx
+++ react-album/src/pages/index/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import styles from './styles/index.module.scss'
 
 function index() {
     return(
-        <div>Main Page</div>
+        <div className={styles.page}>
+            Main Page
+            {/** 공통 헤더 UI 파트*/}
+            {/** 공통 네비게이션 UI 파트 */}
+            <div className={styles.page__contents}>
+                <div className={styles.page__contents__introBox}>
+                    <div className={styles.wrapper}>
+                        <span className={styles.wrapper__title}>PhotoSplash</span>
+                        <span className={styles.wrapper__desc}>
+                            It is an Album that gives splash of photo. <br/>
+                            Enjoy!
+                        </span>
+                        {/** 검색창 UI 파트 */}
+                    </div>
+                </div>
+                <div className={styles.page__contents__imageBox}>
+                </div>
+            </div>
+            {/** 공통 footer UI 파트 */}
+        </div>
     );
 };
 
